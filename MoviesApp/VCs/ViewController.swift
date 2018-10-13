25549795591d9d016e72ef8ebbd30d904de3a3b9
Mostafa AbdEl Fatah <mostafaabdelfatah@mostafas-mac.local>
2018-10-12 18:25:33 -0700

    some change

--- MoviesApp/VCs/ViewController.swift
+++ MoviesApp/VCs/ViewController.swift
@@ -11,13 +11,13 @@ import Alamofire
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var indexPath:IndexPath?
     var moviesList:[Movie] = []
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.refresh()
+        
     }
 
     @IBAction func refresh_btnClicked(_ sender: UIBarButtonItem) {
@@ -25,11 +25,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func refresh(){
+        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        loadingIndicator.startAnimating();
+        
+        alert.view.addSubview(loadingIndicator)
+        self.present(alert, animated: true, completion: nil)
+        
         API.fetchMovies { movies in
+            
             if movies != nil {
                 self.moviesList.removeAll()
                 self.moviesList += movies!
                 self.tableView.reloadData()
+                self.dismiss(animated: false, completion: nil)
             }
         }
     }
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '9.0'
+use_frameworks!
+target 'MoviesApp' do
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'RealmSwift'
+  pod "youtube-ios-player-helper"
+
+end
